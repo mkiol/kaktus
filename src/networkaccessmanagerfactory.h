58adc6d63ef9b10c1e38b892ba39af81d93c0d4f
Muki <michal@mkiol.net>
2015-12-01 21:22:23 +0100

    [Sailfish] BUG FIX: Icons are not displayed in offline mode

--- src/networkaccessmanagerfactory.h
+++ src/networkaccessmanagerfactory.h
@@ -1,14 +1,24 @@
 #ifndef NETWORKACCESSMANAGERFACTORY_H
 #define NETWORKACCESSMANAGERFACTORY_H
 
+#include <QNetworkAccessManager>
+#include <QString>
+
+#if QT_VERSION >= QT_VERSION_CHECK(5,0,0) // QT5
+#include <QQmlNetworkAccessManagerFactory>
+
+class NetworkAccessManagerFactory: public QQmlNetworkAccessManagerFactory
+{
+#else // QT5
 #include <QDeclarativeNetworkAccessManagerFactory>
 
 class NetworkAccessManagerFactory: public QDeclarativeNetworkAccessManagerFactory
 {
+#endif // QT5
 
 public:
     NetworkAccessManagerFactory(QString userAgent);
-    QNetworkAccessManager *create(QObject* parent);
+    virtual QNetworkAccessManager *create(QObject* parent);
 
 private:
     QString userAgent;
